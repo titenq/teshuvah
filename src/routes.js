a61d04b46b9d9596525ef3a386024e74d71eee55
TitenQ <titenq@gmail.com>
2020-05-24 18:03:16 -0300

    lazy loading

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Biblia from './pages/Biblia';
-import Parashat from './pages/Parashat';
-import Dicionario from './pages/Dicionario';
-import Login from './pages/Login';
-import PageNotFound from './pages/PageNotFound';
+const Home = lazy(() => import('./pages/Home'));
+const Biblia = lazy(() => import('./pages/Biblia'));
+const Parashat = lazy(() => import('./pages/Parashat'));
+const Dicionario = lazy(() => import('./pages/Dicionario'));
+const Login = lazy(() => import('./pages/Login'));
+const PageNotFound = lazy(() => import('./pages/PageNotFound'));
 
-export default function Routes() {
+const Routes = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/biblia" component={Biblia} />
-      <Route exact path="/parashat" component={Parashat} />
-      <Route exact path="/dicionario" component={Dicionario} />
-      <Route exact path="/login" component={Login} />
-      <Route component={PageNotFound} />
-    </Switch>
+    <Suspense fallback={<h1>Carregando...</h1>}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/biblia" component={Biblia} />
+        <Route exact path="/parashat" component={Parashat} />
+        <Route exact path="/dicionario" component={Dicionario} />
+        <Route exact path="/login" component={Login} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </Suspense>
   );
-}
+};
+
+export default Routes;
